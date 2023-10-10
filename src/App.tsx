ee134d813fc7dc0129c0abfe4fffe240387fc864
Sebastian <mrseb10@gmail.com>
2023-10-10 21:47:06 +0200

    Smoother animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Timer from './components/Timer';
 
 function App() {
 	return (
 		<Fragment>
 			<Header />
 			<Navbar />
+			<Timer />
 		</Fragment>
 	);
 }
---
+++ src/components/Timer.tsx
@@ -0,0 +1,11 @@
+// import styles from '../scss/Timer.module.scss';
+
+const Timer = () => {
+	return (
+		<div>
+			<div></div>
+		</div>
+	);
+};
+
+export default Timer;
