ca37acd6da370d26d01e9ce88605b2567e573fee
Sebastian <mrseb10@gmail.com>
2023-10-11 18:56:11 +0200

    Nested Time inside Timer for readability

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -4,39 +4,38 @@ import styles from '../scss/Timer.module.scss';
 
 import useSettings from '../hooks/useSettings';
 
-const Time = () => {
+const Timer = () => {
 	const { color, font } = useSettings();
 
-	let hoverClass: string = '';
-	switch (color) {
-		case '#f87070':
-			hoverClass = 'action__red';
-			break;
-		case '#70f3f8':
-			hoverClass = 'action__cyan';
-			break;
-		case '#d881f8':
-			hoverClass = 'action__pink';
-			break;
+	function Time() {
+		let hoverClass: string = '';
+		switch (color) {
+			case '#f87070':
+				hoverClass = 'action__red';
+				break;
+			case '#70f3f8':
+				hoverClass = 'action__cyan';
+				break;
+			case '#d881f8':
+				hoverClass = 'action__pink';
+				break;
+		}
+
+		return (
+			<div className={styles.time__container}>
+				<h1 style={{ fontFamily: font }} className={styles.time}>
+					00:00
+				</h1>
+				<button
+					style={{ fontFamily: font }}
+					className={`${styles.action} ${hoverClass}`}
+				>
+					Start
+				</button>
+			</div>
+		);
 	}
 
-	return (
-		<div className={styles.time__container}>
-			<h1 style={{ fontFamily: font }} className={styles.time}>
-				00:00
-			</h1>
-			<h3
-				style={{ fontFamily: font }}
-				className={`${styles.action} ${hoverClass}`}
-			>
-				Start
-			</h3>
-		</div>
-	);
-};
-
-const Timer = () => {
-	const { color } = useSettings();
 	return (
 		<div className={styles.timer__outer}>
 			<div className={styles.timer__inner}>
